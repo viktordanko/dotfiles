ed0ea0c7fa44929ad549f2ace548d6cdff101a6d
Viktor Daňko <viktor.danko@superkoders.com>
2021-07-13 06:38:51 +0200

    Setup update

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+screenshots_location=~/Documents/Screenshots/
+
 # Setup folder for projects
-mkdir ~/htdocs
+mkdir ~/Projects
 
 # Install brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -18,20 +20,26 @@ brew install gh
 brew install npm
 brew install yarn
 brew install volta
+brew install git
+brew install fzf
 
 # Software
-brew cask install visual-studio-code
-brew cask install iterm2
-brew cask install notion
-brew cask install discord
-brew cask install vlc
-brew cask install spotify
-brew cask install steam
+brew install --cask visual-studio-code
+brew install --cask iterm2
+brew install --cask notion
+brew install --cask forklift
+brew install --cask alfred
+brew install --cask figma
+brew install --cask discord
+brew install --cask vlc
+brew install --cask spotify
+brew install --cask steam
 
 # Setup settings
-git clone https://github.com/viktordanko/dotfiles ~/htdocs/_dotfiles
-rm -rf ~/Library/ApplicationSupport/Code/User
-ln -s ~/htdocs/_dotfiles/vscode ~/Library/ApplicationSupport/Code/User
+git clone https://github.com/viktordanko/dotfiles ~/Projects/_dotfiles
+rm -rf ~/Library/Application\ Support/Code/User
+ln -s ~/Projects/vscode ~/Library/Application\ Support/Code/User
+ln -s ~/Projects/.config/config.fish ~/.config/fish/config.fish
 
 # Show path in finder
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool true; killall Finder
