5a853aa1b51b3770b361489de848e6341b4230a2
Viktor Daňko <viktor.danko@superkoders.com>
2021-09-09 09:32:31 +0200

    Updated by Gbck

--- setup.sh
+++ setup.sh
@@ -7,6 +7,8 @@ mkdir ~/Projects
 
 # Install brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/viktordanko/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # Fish
 brew install fish
